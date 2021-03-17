8f73067dba4c14cc071872e47dce88b230dfb249
Babitababy <kumaribabita2402@gmail.com>
2021-03-17 18:49:26 +0530

    fetching product from database

--- mycart/shop/views.py
+++ mycart/shop/views.py
@@ -3,8 +3,16 @@ from django.shortcuts import render
 # Create your views here.
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product
+from math import ceil
+
 def index(request):
-    return render(request,'shop/index.html')
+    products= Product.objects.all()
+    print(products)
+    num = len(products)
+    number_slides=num//4 + ceil((num/4)-(num//4))
+    params= {'no_of_slides':number_slides,'range':range(1,number_slides),'product':products}
+    return render(request,'shop/index.html',params)
 
 def about(request):
     return render(request,"shop/about.html")
