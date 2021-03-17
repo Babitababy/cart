946b28884779f2813bfd3c31697e6f368d315824
Babitababy <kumaribabita2402@gmail.com>
2021-03-17 17:48:49 +0530

    basic and about page created

--- mycart/shop/views.py
+++ mycart/shop/views.py
@@ -7,7 +7,7 @@ def index(request):
     return render(request,'shop/index.html')
 
 def about(request):
-    return HttpResponse("ABOUT")
+    return render(request,"shop/about.html")
 
 def contact(request):
     return HttpResponse("CONTACT")
