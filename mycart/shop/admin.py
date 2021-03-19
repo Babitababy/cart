1a92ae41186a9cf0016fb320300f43d054bd974e
Babitababy <kumaribabita2402@gmail.com>
2021-03-19 09:27:52 +0530

    table created

--- mycart/shop/admin.py
+++ mycart/shop/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Product, Contact
 
-from .models import Product
 admin.site.register(Product)
+admin.site.register(Contact)
+
 
