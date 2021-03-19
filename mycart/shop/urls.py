92b9e33acd47f43a93ca4aba590463509381139a
Babitababy <kumaribabita2402@gmail.com>
2021-03-19 09:07:00 +0530

    product view page created

--- mycart/shop/urls.py
+++ mycart/shop/urls.py
@@ -6,11 +6,11 @@ from . import views
 urlpatterns = [
 
     path("",views.index,name="ShopHome"),
-    path("about/",views.about,name="AboutUs"),
+    path("about/", views.about, name="AboutUs"),
     path("contact/", views.contact, name="ContactUs"),
     path("tracker/", views.tracker, name="TrackingStatus"),
     path("search/", views.search, name="Search"),
-    path("productView/", views.productView, name="productView"),
+    path("products/<int:myid>", views.productView, name="ProductView"),
     path("checkout/", views.checkout, name="Checkout"),
 
 ]
